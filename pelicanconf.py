0319e2f9fbcc732673b4e231dc2db50bce6b3989
shadowyogi <m.iron@icloud.com>
2022-07-15 19:03:09 +0200

    flex is kindly enough to provide an adfree experience

--- pelicanconf.py
+++ pelicanconf.py
@@ -82,7 +82,7 @@ CC_LICENSE = {
 COPYRIGHT_YEAR = datetime.now().year
 DEFAULT_PAGINATION = 3
 
-DISQUS_SITENAME = "street-yoga"
+DISQUS_SITENAME = "flex-pelican"
 ADD_THIS_ID = "ra-62d18ff205e38df4"
 
 STATIC_PATHS = ["images"]
